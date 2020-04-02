c47668030a1a46a06668e67bdb9d12ee4324ec96
liu-ls <1418249866@qq.com>
2020-04-02 14:28:34 +0800

    add check cookies

--- src/main/java/life/school/community/mapper/UserMapper.java
+++ src/main/java/life/school/community/mapper/UserMapper.java
@@ -3,9 +3,13 @@ package life.school.community.mapper;
 import life.school.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(String token);
 }
